4114323e608f08f32282bbf591fc79658bc99b8b
Your Name <you@example.com>
2018-12-13 20:11:59 +0100

    check class availability to ensure that match data can be loaded

--- src/main/java/com/rohanch/bmonoddsscraper/pages/InPlay.java
+++ src/main/java/com/rohanch/bmonoddsscraper/pages/InPlay.java
@@ -8,22 +8,35 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class InPlay {
-	public static void OpenSportOnName(WebDriver webDriver, String sportName) throws Exception {
+	public static void OpenSportOnName(WebDriver webDriver, String sportName) {
 		var sportSelectElement = webDriver.findElement(By.xpath(String.format("//%s[text()=\"%s\"]", "div", sportName)));
 
 		System.out.printf("Clicking sport: %s in InPlay page...\n", sportName);
 		sportSelectElement.click();
 	}
 
-	public static void ScrapeLiveGamesData(WebDriver webDriver, String sportName, String marketName) throws Exception {
+	public static Match[] ScrapeLiveGamesData(WebDriver webDriver, String sportName, String marketName) throws Exception {
+		if (!hasAvailableMatchesForCurrentSportName(webDriver)) {
+			throw new Exception(String.format("No lives games currently available for: %s\n", sportName));
+		}
+
 		var expression = Inject.constructJSExpressionWithFile(String.format("return ReadOddDataOnBasicMarket(\"%s\",\"%s\");", sportName, marketName), "assets\\inject\\inplay-matches-helper.js");
 
 		var res = ((JavascriptExecutor) webDriver).executeScript(expression);
 		if (res == null) {
-			throw new Exception(String.format("could not fetch current live games data for: %s\n", sportName));
+			throw new Exception(String.format("Could not fetch current live games data for: %s\n", sportName));
 		}
 
 		ObjectMapper mapper = new ObjectMapper();
-		Match[] matches = mapper.convertValue(res, Match[].class);
+		return mapper.convertValue(res, Match[].class);
+	}
+
+	private static boolean hasAvailableMatchesForCurrentSportName(WebDriver webDriver) {
+		try {
+			webDriver.findElement(By.className("ipo-Fixture_TableRow"));
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 }
