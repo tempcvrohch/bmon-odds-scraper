2c3fe2fab797f0a7373e70f4e21c83eebd8b1108
Your Name <you@example.com>
2018-12-15 17:05:21 +0100

    added initial lazy load of current/recent matches

--- src/main/java/com/rohanch/bmonoddsscraper/repositories/MatchRepository.java
+++ src/main/java/com/rohanch/bmonoddsscraper/repositories/MatchRepository.java
@@ -2,8 +2,15 @@ package com.rohanch.bmonoddsscraper.repositories;
 
 import com.rohanch.bmonoddsscraper.models.db.MatchEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @Repository
 public interface MatchRepository extends JpaRepository<MatchEntity, Long> {
+	@Query(value = "SELECT * FROM matches m WHERE m.created_at >= :timestamp", nativeQuery = true)
+	List<MatchEntity> findAfterTimestamp(@Param("timestamp") Timestamp timestamp);
 }
