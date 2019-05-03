36f060452db9ce35b60f4e8dda82abcb89cb7838
Your Name <you@example.com>
2019-05-03 21:47:21 +0200

    formatting marketstaterepository

--- src/main/java/com/rohanch/bmonoddsscraper/repositories/MarketStateRepository.java
+++ src/main/java/com/rohanch/bmonoddsscraper/repositories/MarketStateRepository.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface MarketStateRepository extends JpaRepository<MarketState, Long> {
 	//TODO: there is probably a better way of doing this.
-	@Query(value = "SELECT mts.* FROM matches m JOIN matches_states ms ON m.id = ms.match_id JOIN market_states mts ON ms.id = mts.match_state_id WHERE m.id = :matchId ORDER BY mts.id DESC LIMIT 2 ", nativeQuery = true)
+	@Query(value = "SELECT mts.* FROM matches m " +
+			"JOIN matches_states ms ON m.id = ms.match_id " +
+			"JOIN market_states mts ON ms.id = mts.match_state_id " +
+			"WHERE m.id = :matchId ORDER BY mts.id DESC LIMIT 2 ", nativeQuery = true)
 	MarketState[] findLatestMarketStatesOnMatchId(@Param("matchId") Long matchId);
 }
