988c92a1a8f99a990cd880f6383b114f3bddc0fd
Your Name <you@example.com>
2019-05-03 21:49:27 +0200

    added void bet scenario to repository

--- src/main/java/com/rohanch/bmonoddsscraper/repositories/UserRepository.java
+++ src/main/java/com/rohanch/bmonoddsscraper/repositories/UserRepository.java
@@ -10,6 +10,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUsername(String username);
 
 	@Modifying
-	@Query(value = "UPDATE users SET balance = balance - :stake WHERE id = :userId", nativeQuery = true)
-	void reduceBalanceByUsername(@Param("userId") Long userId, @Param("stake") float stake);
+	@Query(value = "UPDATE users SET balance = balance - :amount WHERE id = :userId", nativeQuery = true)
+	void reduceBalanceByUsername(@Param("userId") Long userId, @Param("amount") float amount);
+
+	@Modifying
+	@Query(value = "UPDATE users SET balance = balance + :amount WHERE id = :userId", nativeQuery = true)
+	void incrementBalanceByUsername(@Param("userId") Long userId, @Param("amount") float amount);
 }
